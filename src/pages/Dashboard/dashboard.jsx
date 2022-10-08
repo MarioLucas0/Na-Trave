d4547f6863ebe4da5d87974aa422e9d3e82e1374
Lucas <mariolucas.2000@outlook.com>
2022-10-08 19:59:11 -0300

    :bug: bug fix on dashboard screen

--- src/pages/Dashboard/dashboard.jsx
+++ src/pages/Dashboard/dashboard.jsx
@@ -6,8 +6,8 @@ import { Icon } from '../../components/icon/Icon'
 
 export const Dashboard   = () => (
   <div className="">
-     
-     <header className="bg-red-500 text-white p-4">
+
+      <header className="bg-red-500 text-white p-4">
         <div  className="container max-w-3xl  flex justify-between ">
           <img src={logo}  className="w-28 md:w-40" />
           <Link to="/profile">
@@ -25,21 +25,31 @@ export const Dashboard   = () => (
           </div>
         </section>
 
-        <section id="content" className="container max-w-3xl p-4 space-y-4">
+        <section id="content" className="container max-w-3xl p-4">
             
-           <Card 
-           
-           timeA={{slug: 'sui' }}
-           timeB={{slug: 'cam' }}
-           match={{time: '7:00'}}
-           
-           
-           />
+            
+            <div className="p-4 flex space-x-4 items-center  justify-center">
+               <Icon name="arrowLeft" className="w-6 text-red-500"></Icon>
+                <span className="font-bold">24 de novembro</span>
+                <Icon name="arrowRight" className="w-6 text-red-500"></Icon>
+            </div>
+
+            <div className="space-y-4">
+            <Card 
+
+            timeA={{slug: 'sui' }}
+            timeB={{slug: 'cam' }}
+            match={{time: '7:00'}}
+            
+            
+            />
+            </div>
+
 
 
         </section>
 
       </main>
-     
+
   </div>
 )
